00a18257ba2276a388b79901648e024a5160cc3f
Michał Mistarz <michal.mistarz02@gmail.com>
2024-05-12 21:54:36 +0200

    Adds option to display choosen by user game

--- games/src/main/java/dev/mistarz/games/GameController.java
+++ games/src/main/java/dev/mistarz/games/GameController.java
@@ -1,14 +1,17 @@
 package dev.mistarz.games;
 
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/games")
@@ -20,5 +23,10 @@ public class GameController {
     @GetMapping
     public ResponseEntity<List<Game>> getAllGames(){
         return new ResponseEntity<List<Game>>(gameService.allGames(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{metacritic}")
+    public ResponseEntity<Optional<Game>> getSingleGame(@PathVariable String metacritic){
+        return new ResponseEntity<Optional<Game>>(gameService.signleGame(metacritic), HttpStatus.OK);
     }
 }
--- games/src/main/java/dev/mistarz/games/GameService.java
+++ games/src/main/java/dev/mistarz/games/GameService.java
@@ -1,9 +1,11 @@
 package dev.mistarz.games;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService{
@@ -13,5 +15,9 @@ public class GameService{
 
     public List<Game> allGames() {
         return gameRepository.findAll();        // findAll from mongo repository class
+    }
+
+    public Optional<Game> signleGame(String metacritic){
+        return gameRepository.findGameByMetacritic(metacritic);
     }
 }
