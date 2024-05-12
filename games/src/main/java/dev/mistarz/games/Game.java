7785028f431b2b2cff76d6079886bdb71a674c62
Michał Mistarz <michal.mistarz02@gmail.com>
2024-05-12 20:24:09 +0200

    Adds basic structure of Game class

--- games/src/main/java/dev/mistarz/games/Game.java
+++ games/src/main/java/dev/mistarz/games/Game.java
@@ -1,4 +1,31 @@
 package dev.mistarz.games;
 
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "games")
+@Data                                   //  @Data takes care about setters and getters, string methods
+@AllArgsConstructor                     // constructor takes all these private variables as argument
+@NoArgsConstructor
 public class Game {
+
+    @Id                                 // @id => unique id for every game
+    private ObjectId id;
+    private String metacritic;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private List<String> genres;
+    private String poster;
+    private String developer;
+    private List<String> backdrops;
+    private List<String> platforms;
+
 }
+
