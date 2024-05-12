a054c3f7bb09b7a271d8e1892da43ddff39acf82
Michał Mistarz <michal.mistarz02@gmail.com>
2024-05-12 21:23:19 +0200

    Gives option to print games from db

--- games/src/main/java/dev/mistarz/games/GameController.java
+++ games/src/main/java/dev/mistarz/games/GameController.java
@@ -1,18 +1,24 @@
 package dev.mistarz.games;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/games")
 public class GameController {
 
+    @Autowired
+    private GameService gameService;
+
     @GetMapping
-    public ResponseEntity<String> allGames(){
-        return new ResponseEntity<String>("All Games!", HttpStatus.OK);
+    public ResponseEntity<List<Game>> getAllGames(){
+        return new ResponseEntity<List<Game>>(gameService.allGames(), HttpStatus.OK);
     }
 }
--- games/src/main/java/dev/mistarz/games/GameService.java
+++ games/src/main/java/dev/mistarz/games/GameService.java
@@ -1,4 +1,17 @@
 package dev.mistarz.games;
 
-public class GameService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService{
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<Game> allGames() {
+        return gameRepository.findAll();        // findAll from mongo repository class
+    }
 }
